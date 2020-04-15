ca1b60e7773c8e999291e8b8cf41b77140641e69
Admin <Admin@FlyingMachine>
2020-04-15 14:12:53 +0530

    first commit

---
+++ test/src/com/cg/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.cg;
+ 
+/**
+ *  My Hello World class
+ * @author Hima Varsha
+ * @version 1.0
+ */
+public class  HelloWorld implements Hello {
+    @Override
+    public String sayHello() {
+    	return "Hello World";
+    }
+}
